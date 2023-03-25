c5554da95fcab9096b0bbd181dae9141642bf95d
Kunal Gupta <32365955+Kumagi360@users.noreply.github.com>
2023-03-25 17:48:28 -0400

    implements working current chop

--- MContV6_2.ino
+++ MContV6_2.ino
@@ -15,27 +15,26 @@
 #define HALL_C_PIN 5
 
 // spare PWM pins
-//#define PWM1 6
+#define PWM1 6
 #define PWM2 7
-//#define PWM3 8
+#define PWM3 8
 
 // spare analog pins
-#define ANLG1 A1
+#define ANLG1 A3
 #define ANLG2 A2
-#define ANLG3 A3
+#define ANLG3 A1
 
 // driver control pins
-#define AH_PIN 6 // formerly 10    
+#define AH_PIN 10   
 #define AL_PIN 12
-#define BH_PIN 8 // formerly 45
+#define BH_PIN 45 
 #define BL_PIN 46
 #define CH_PIN 44
-#define CL_PIN 43
+#define CL_PIN 43 
 
 // overcurrent (active high) pin
-#define OVERC 42
+#define OVERC 21 // formerly 42 (no interrupt)
 
-// 
 #define THROTTLE_PIN A4      
 
 #define LED_PIN 22
@@ -43,39 +42,36 @@
 // oversampling for noise filtering during nominal operation. increase if skipping/cogging at low speed
 int HALL_OVERSAMPLE = 10;
 
+
+uint8_t motorState;
+
 // mapping hall sensing to motor state
 //uint8_t hallToMotor[8] = {255, 255, 255, 255, 255, 255, 255, 255}; // default hall to motor state mapping, unused
 uint8_t hallToMotor[8] = {255, 4, 2, 3, 0, 5, 1, 255}; // correct direction
-//uint8_t hallToMotor[8] = {255, 1, 3, 2, 5, 0, 4, 255}; // reverse direction NOT WORKING
-//uint8_t hallToMotor[8] = {255, 3, 5, 4, 1, 2, 0, 255}; // reverse direction
 
 // writes pwm to high side FETs, digital value to low side
 void writePhases(uint8_t ah, uint8_t bh, uint8_t ch, uint8_t al, uint8_t bl, uint8_t cl)
 {
 
-  digitalWriteFast(AH_PIN, 0);
-  digitalWriteFast(BH_PIN, 0);
-  digitalWriteFast(CH_PIN, 0);
-
-  digitalWriteFast(AL_PIN, al);
-  digitalWriteFast(BL_PIN, bl);
-  digitalWriteFast(CL_PIN, cl);
+  // digitalWriteFast(AL_PIN, 0);
+  // digitalWriteFast(BL_PIN, 0);
+  // digitalWriteFast(CL_PIN, 0);
 
   analogWrite(AH_PIN, ah);
   analogWrite(BH_PIN, bh);
   analogWrite(CH_PIN, ch);
 
-  // digitalWriteFast(AL_PIN, al);
-  // digitalWriteFast(BL_PIN, bl);
-  // digitalWriteFast(CL_PIN, cl);
+  digitalWriteFast(AL_PIN, al);
+  digitalWriteFast(BL_PIN, bl);
+  digitalWriteFast(CL_PIN, cl);
+
 }
 
 // sets corresponding phases to be written given current motor state. if invlaid state, kills all FETs and re-identifies halls.
 void writePWM(uint8_t motorState, uint8_t dutyCycle)
 {
-
   
-  // Working clockwise viewing with sprocket then motor
+  // spin shaft clockwise viewing from shaft to motor
   if(motorState == 0)                         // LOW A, HIGH B
       writePhases(0, dutyCycle, 0, 1, 0, 0);
   else if(motorState == 1)                    // LOW A, HIGH C
@@ -88,32 +84,16 @@ void writePWM(uint8_t motorState, uint8_t dutyCycle)
       writePhases(dutyCycle, 0, 0, 0, 0, 1);
   else if(motorState == 5)                    // LOW C, HIGH B
       writePhases(0, dutyCycle, 0, 0, 0, 1);
-
-  // // Untested counterclockwise viewing with sprocket then motor
-  // if(motorState == 5)                         // LOW A, HIGH B
-  //     writePhases(0, dutyCycle, 0, 1, 0, 0);
-  // else if(motorState == 4)                    // LOW A, HIGH C
-  //     writePhases(0, 0, dutyCycle, 1, 0, 0);
-  // else if(motorState == 3)                    // LOW B, HIGH C
-  //     writePhases(0, 0, dutyCycle, 0, 1, 0);
-  // else if(motorState == 2)                     // LOW B, HIGH A
-  //     writePhases(dutyCycle, 0, 0, 0, 1, 0);
-  // else if(motorState == 1)                    // LOW C, HIGH A
-  //     writePhases(dutyCycle, 0, 0, 0, 0, 1);
-  // else if(motorState == 0)                    // LOW C, HIGH B
-  //     writePhases(0, dutyCycle, 0, 0, 0, 1);
-
-  else{                                        // All off
+  else{                                      
     writePhases(0, 0, 0, 0, 0, 0);
     
+    // hang program and express failure condition
     while(true){
       digitalWriteFast(LED_PIN, HIGH);
       delay(1000);
       digitalWriteFast(LED_PIN, LOW);
       delay(1000);
-    }
-    //identifyHalls();
-      
+    }    
   }
 }
 
@@ -141,7 +121,7 @@ uint8_t getHalls()
   return hall & 0x7;                            // Just to make sure we didn't do anything stupid, set the maximum output value to 7
 }
 
-/* Magic function to do hall auto-identification. Moves the motor to all 6 states, then reads the hall values from each one
+/* Auto-identifcation. Moves the motor to all 6 states, then reads the hall values from each one
  * 
  * Note, that in order to get a clean hall reading, we actually need to commutate to half-states. So instead of going to state 3, for example
  * we commutate to state 3.5, by rapidly switching between states 3 and 4. After waiting for a while (half a second), we read the hall value.
@@ -205,18 +185,16 @@ void identifyHalls()
 }
 
 
-/* Read the throttle value from the ADC. Because our ADC can read from 0v-3.3v, but the throttle doesn't output over this whole range,
- * scale the throttle reading to take up the full range of 0-255
- */
 uint8_t readThrottle()
 {
+  // read throttle and rescale for /255
   int adc = analogRead(THROTTLE_PIN); 
-  adc = adc/4; //rescale for /255
+  adc = adc/4;
   
-  // bound/processing if necessary
+  // eliminate zero offset if present / eliminate low PWM range
   adc = (adc - THROTTLE_LOW);
-  //adc = adc*2;
 
+  // bound throttle to prevent overflow
   if (adc > 254)
     return 254;
 
@@ -224,56 +202,41 @@ uint8_t readThrottle()
     return 0;
 
   return adc;
-
-  // ** static throttle input for testing ** //
-  // uint8_t adc = 140;
-  // if (random(0, 100) > 80) {
-  //   adc = 80;
-  // }
-  //return adc;
 }
 
-void checkOverC(int motorState){
-  if (digitalReadFast(OVERC)){
-      unsigned long start;
+void checkOverC(){
+  // uint8_t start = micros();
+  digitalWriteFast(LED_PIN, HIGH);
+  digitalWriteFast(AH_PIN, 0);
+  digitalWriteFast(BH_PIN, 0);
+  digitalWriteFast(CH_PIN, 0);
 
-    digitalWriteFast(LED_PIN, HIGH);
+  // if (digitalReadFast(OVERC)){
 
-    if (motorState == 0 | motorState == 5){
-      digitalWriteFast(BH_PIN, 0);
-      // start = micros();
-      // while (micros() - start < 5){}
-      digitalWriteFast(BL_PIN, 1);
-    }
-    else if (motorState == 1 | motorState == 2){
-      digitalWriteFast(CH_PIN, 0);
-      // start = micros();
-      // while (micros() - start < 5){}
-      digitalWriteFast(CL_PIN, 1);
-    }
-    else if (motorState == 3 | motorState == 4){
-      digitalWriteFast(AH_PIN, 0);
-      // start = micros();
-      // while (micros() - start < 5){}
-      digitalWriteFast(AL_PIN, 1);
-    }
-  } 
-  else{
-    digitalWriteFast(LED_PIN, LOW);
-  }
+
+
+  //   // if (motorState == 0 | motorState == 5){
+  //   //   digitalWriteFast(BH_PIN, 0);
+  //   //   //while (micros() - start < 1){}
+  //   //   //digitalWriteFast(BL_PIN, 1);
+  //   // }
+  //   // else if (motorState == 1 | motorState == 2){
+  //   //   digitalWriteFast(CH_PIN, 0);
+  //   //   //while (micros() - start < 1){}
+  //   //   //digitalWriteFast(CL_PIN, 1);
+  //   // }
+  //   // else if (motorState == 3 | motorState == 4){
+  //   //   digitalWriteFast(AH_PIN, 0);
+  //   //   //while (micros() - start < 1){}
+  //   //   //digitalWriteFast(AL_PIN, 1);
+  //   // }
+  // } 
+  // else{
+  //   digitalWriteFast(LED_PIN, LOW);
+  // }
 }
 
 void setup() {
-  // TCCR5A = 0;
-  // TCCR5B = 0;
-  // TCNT5  = 0;
-  // // Mode 10: phase correct PWM with ICR4 as Top (= F_CPU/2/25000)
-  // // OC4C as Non-Inverted PWM output
-  // ICR5   = (F_CPU/32000)/2;
-  // OCR5C  = ICR5/2;                    // default: about 50:50
-  // TCCR5A = _BV(COM5C1) | _BV(WGM51);
-  // TCCR5B = _BV(WGM53) | _BV(CS50);
-
   // Set the PWM pin as output.
   TCCR4B = TCCR4B & B11111000 | B00000001; // set digital 6,7,8 to pwm 31372.55 Hz
   TCCR5B = TCCR5B & B11111000 | B00000001; // set digital 44, 45, 46 to pwm 31372.55 Hz
@@ -292,7 +255,13 @@ void setup() {
   digitalWriteFast(LED_PIN, LOW);
   delay(1000);
 
+  pinModeFast(PWM1, OUTPUT);
   pinModeFast(PWM2, OUTPUT);
+  pinModeFast(PWM3, OUTPUT);
+
+  pinModeFast(ANLG1, OUTPUT);
+  pinModeFast(ANLG2, OUTPUT);
+  pinModeFast(ANLG3, OUTPUT);
 
   pinModeFast(AH_PIN, OUTPUT);
   pinModeFast(AL_PIN, OUTPUT);
@@ -309,24 +278,22 @@ void setup() {
   // throttle pin pulled down on board
   pinModeFast(THROTTLE_PIN, INPUT);
 
-  // overcurrent pin is input
-  pinModeFast(OVERC, INPUT);
+  // Formerly overcurrent pin is input, now interrupt
+  //pinModeFast(OVERC, INPUT);
+  pinMode(OVERC, INPUT);
+  attachInterrupt(digitalPinToInterrupt(OVERC), checkOverC, RISING); // commmenting this disables interrupts
 
-  //identifyHalls(); // uncomment for hall identification on startup. suggest doing auto-identify unloaded, import vals into mapping array for loaded
+  // Uncomment for hall identification on startup. suggest doing auto-identify unloaded, import vals into mapping array for loaded.
+  //identifyHalls(); 
 
-  // Ramp up sequence
-  uint8_t motorState;
+  // Uncomment for ramp up sequence to constant speed if loop PWM is constant
   uint8_t hall;
-
-  for(uint8_t i = 0; i < 200; i++)
+  for(int i = 0; i < 10000; i++)
   { 
     // read current hall state, map to motor state and drive accordingly
     hall = getHalls();
     motorState = hallToMotor[hall]; 
-    //writePWM(motorState, throttle);
-
-    // testing with one phase
-    writePWM(motorState, i);
+    writePWM(motorState, i/40);
   }
 
 }
@@ -334,44 +301,58 @@ void setup() {
 // vals used in main loop
 uint8_t newThrottle;
 uint8_t throttle = 0;
-uint8_t motorState;
 uint8_t hall;
 int iterator = 0;
 
 void loop() {
 
-  // iterator = (iterator + 1) % 100;
-  // if (iterator == 0){
-  //   digitalWriteFast(LED_PIN, !digitalReadFast(LED_PIN));
-  // }
+  // uncomment for verification of hitting main loop
+  // writePWM(255, 0);
+  // while (true){}
 
+  // Uncomment for debugging hall connections
   //Serial.println(hallToMotor[getHalls()]);
   //Serial.println(getHalls());
   //Serial.println(readThrottle());
-
-  // // need to ensure new throttle doesn't suddenly spike PWM, (max spike as of now, 255/2 -> 177
-  throttle = readThrottle();
-  // //newThrottle = readThrottle();  
-  // // if (newThrottle - throttle > 50){
-  // //   throttle = throttle + 50;
-  // // }
-  // // else{
-  // //   throttle = newThrottle;
-  // // }
   
-  // // readThrottle() is slow. So do the more important things 200 times more often
+  digitalWriteFast(LED_PIN, LOW);
+  
+  // prevent spiking of current due to sudden changes in throttle
+  // newThrottle = readThrottle();
+  // if (newThrottle > throttle){
+  //   if (throttle < newThrottle - 10){
+  //     throttle = throttle + 10;
+  //   }
+  //   else{
+  //     throttle = newThrottle;
+  //   }
+  // }
+  // else{
+  //   if (newThrottle < throttle - 10){
+  //     throttle = throttle - 10;
+  //   }
+  //   else{
+  //     throttle = newThrottle;
+  //   }
+  // }
+
+  
+  // for every 200 PWM writes, update throttle
   for(uint8_t i = 0; i < 200; i++)
   { 
 
     // read current hall state, map to motor state and drive accordingly
     hall = getHalls();
     motorState = hallToMotor[hall]; 
-    checkOverC(motorState);
 
-    //writePWM(motorState, throttle);
+    // uncomment to verify overcurrent pin live
+    //checkOverC();
 
-    // testing with one phase
-    writePWM(motorState, 200);
+    // uncomment for set speed
+    writePWM(motorState, 250);
+
+    // uncomment for throttle speed
+    // writePWM(motorState, throttle);
   }
   
 }
